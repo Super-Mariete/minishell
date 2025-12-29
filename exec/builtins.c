603f226cd23f4b24fd85dffce8f495c8d4b88f55
rms35 <rms34@gmail.com>
2025-12-29 01:04:44 +0100

    fixed(exec/builtins.c): various fixes in ft_unset and subroutines

--- exec/builtins.c
+++ exec/builtins.c
@@ -1,60 +1,93 @@
 #include "../includes/minishell.h"
 
-static t_env	*ft_add_empty_var(t_env	*env, t_env *node)
+static void	ft_free_node(t_env *node)
+{
+	ft_bzero((void *)node->key, ft_strlen(node->key));
+	ft_bzero((void *)node->value, ft_strlen(node->value));
+	node->key = NULL;
+	node->value = NULL;
+	node->is_exported = 0;
+}
+
+// static	int	ft_is_gap(const char *buffer)
+// {
+// 	int	i;
+// 	int	j;
+
+// 	i = 0;
+// 	while (i < ARG_MAX)
+// 	{
+// 		if (buffer[i] == 0 && (++i) < ARG_MAX && buffer[i] == 0)
+// 		{
+// 			if (i > 0 && buffer[i - 1] != 0)
+// 				return (0);
+// 			j = 0;
+// 			while ((i + j) <= ARG_MAX)
+// 			{
+// 				if ((i + j) == ARG_MAX)
+// 					return (-1);
+// 				if (buffer[i + j] != 0)
+// 					return (j);
+// 				j++;
+// 			}
+// 		}
+// 		i++;
+// 	}
+// 	return (0);
+// }
+
+static void	ft_add_empty_node(t_env *env, t_env *node)
 {
 	t_env	*temp;
-	t_env	*prev;
 	int	i;
 
 	i = 0;
-	prev = NULL;
 	temp = env;
-	while (i < VAR_MAX && node)
+	while (i < VAR_MAX && temp)
 	{
-		if (!temp->key && prev)
+		if (!temp->key && node != temp)
 		{
-			prev->next = node;
-			node->next = temp;
-			return (env);
-		}
-		if (!temp->key && !prev)
-		{
+			node->next = temp->next;
 			temp->next = node;
-			return (temp);
+			return ;
 		}
-		prev = temp;
+		if (!temp->next)
+			break ;
 		temp = temp->next;
 		i++;
 	}
 	temp->next = node;
 	node->next = NULL;
-	return (env);
+	return ;
 }
 
 void ft_unset(t_msh *msh, char *key)
 {
-	int	i;
 	t_env	*node;
-	t_env	*prev_node;
+	t_env	*next;
 
+	if (!key)
+		return ;
 	node = msh->env;
-	i = 0;
-	prev_node = NULL;
-	while (i < VAR_MAX && node)
+	if (node->key && !ft_strcmp(node->key, key))
 	{
-		if (ft_strcmp(key, node->key))
-		{
-			ft_bzero((void *)node->key, ft_strlen(node->key));
-			ft_bzero((void *)node->value, ft_strlen(node->value));
-			node->is_exported = 0;
-			if (prev_node)
-			{
-				prev_node->next = node->next;
-			}
-			msh->env = ft_add_empty_var(node->next, node);
-		}
-		i++;
-		prev_node = node;
-		node = node->next;
+		ft_free_node(node);
+		ft_add_empty_node(node->next, node);
+		return ;
 	}
+	next = node->next;
+	while (node && next)
+	{
+		if (next->key && !ft_strcmp(next->key, key))
+		{
+			ft_free_node(next);
+			node->next = next->next;
+			ft_add_empty_node(next->next, next);
+			return ;
+		}
+		node = node->next;
+		if (node)
+			next = node->next;
+	}
+	return ;
 }
