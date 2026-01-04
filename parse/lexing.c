ed00e34422c71fd2a0bc96258ac658ecf0744d2f
rms35 <rms34@gmail.com>
2026-01-04 02:28:24 +0100

    feature(parse/): implemented lexing, fails with nested quotes

--- parse/lexing.c
+++ parse/lexing.c
@@ -6,7 +6,7 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 15:23:17 by rafael            #+#    #+#             */
-/*   Updated: 2026/01/03 17:33:06 by rafael           ###   ########.fr       */
+/*   Updated: 2026/01/04 02:07:11 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,13 @@ static size_t	ft_token_len(const char *line)
 {
 	size_t	i;
 	size_t	len;
+	size_t	ret;
 
 	i = 0;
+	ret = 0;
 	len = ft_strlen(line);
 	if (ft_strchr(CONTROL_OP, line[i]))
-		return (i + ft_op_len(line, i));
+		return (ret+ ft_op_len(line, i));
 	while (i < len)
 	{
 		if (ft_strchr(QUOTES, line[i]) && (i == 0 || (i > 0 && line[i - 1] != '\\')))
@@ -38,38 +40,50 @@ static size_t	ft_token_len(const char *line)
 			if (ft_quoted_len(line + i, line[i]) <= 0)
 				return (-1);
 			i = (ft_quoted_len(line + i, line[i]) + i);
+			ret = i;
 			continue ;
 		}
 		if (ft_strchr(METACHARS, line[i]))
-			return (i);
+			return (ret);
 		i++;
+		ret++;
 	}
-	return (i);
+	return (ret);
 }
 
 void	ft_print_buffer(const t_buffer *buff)
 {
-	// char	*t;
 	size_t	i;
 	size_t	r;
+	size_t	p;
 	
 	i = 0;
-	// t = (char *)buffer;
-	printf("buffer:\n");
+	p = 0;
 	r = buff->last - buff->buffer;
 	while (i < r)
 	{
 		if (!buff->buffer[i])
-			printf(" ");
+		{
+			if (i + 1 <= r && !buff->buffer[i + 1])
+			{
+				write(2, "\n", 1);
+				return ;
+			}
+			write(2, " ", 1);
+		}
 		else
-			printf("%c", buff->buffer[i]);
+		{
+			write(2, &(buff->buffer[i]), 1);
+			p++;
+		}
 		i++;
 	}
-	printf("\n");
+	if (p)
+		write(2, "\n", 1);
 	return ;
 }
 
-static int	ft_put_tokens(t_buffer *buff, t_read *read)
+static int	ft_put_tokens(t_buffer *buff, t_read *rbuffer)
 {
 	size_t	i;
 	size_t	len;
@@ -77,20 +91,20 @@ static int	ft_put_tokens(t_buffer *buff, t_read *read)
 
 	i = 0;
 	max = buff->buffer + BUF_MAX - 1;
-	while (i < read->line_len)
+	while (i < rbuffer->line_len)
 	{
-		len = ft_token_len(&(read->buffer[i]));
+		len = ft_token_len(&(rbuffer->buffer[i]));
 		if (!len)
 			break ;
 		if (i + len >= BUF_MAX - 1 || buff->last + len >= max)
 			return (write(2, MEMOUT, sizeof(MEMOUT)), 1);
-		ft_buffercpy(&(read->buffer[i]), buff->last, len);
+		ft_buffercpy(&(rbuffer->buffer[i]), buff->last, len);
 		buff->last += len + 1;
 		buff->current = buff->last;
 		i += len;
-		i += ft_skip_spaces(&(read->buffer[i]));
+		i += ft_skip_spaces(&(rbuffer->buffer[i]));
 	}
-	ft_print_buffer(buff);	
+	ft_print_buffer(buff);
 	return (0);
 }
 
@@ -105,7 +119,7 @@ void	ft_parse(t_msh *msh)
 	buff.current = buffer;
 	buff.last = buffer;
 	msh->buff = &buff;
-	ft_put_tokens(&buff, msh->read);
+	ft_put_tokens(&buff, msh->rbuffer);
 	ft_reset_buffer(&buff);
 	return ;
 }
