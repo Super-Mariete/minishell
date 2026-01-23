66aaf675392173c0c9c190d37fafa5d5c229ef30
rms35 <martinsoeder35@gmail.com>
2026-01-24 00:13:17 +0100

    problemas de paréntesis anidados y pipes en paréntesis. Leaks y crash incluidos

--- parsing/lexing2.c
+++ parsing/lexing2.c
@@ -36,11 +36,11 @@ static bool	check_each_error(char **token, const size_t i)
 	if (token[i] && token[i][0] != 0 && ft_strchr(OP_STR2, token[i][0])
 			&& (token[i + 1] && ft_strchr(OP_STR2, token[i + 1][0])))
 		return (printf("1\n"), perror_token(token[i + 1], SYN_ERR), true);
-	// if (token[i] && token[i][0] == ')' && (token[i + 1]
-	// 		&& !ft_strchr(OP_STR, token[i + 1][0])))
-	// 	return (printf("2\n"), perror_token(token[i + 1], SYN_ERR), true);
-	if (token[i] && token[i][0] == '(' && i > 0
-			&& (!ft_strchr(OP_STR, token[i - 1][0])))
+	if (token[i] && token[i][0] == '(' && (token[i + 1] && token[i + 1][0]
+			&& ft_strchr(OP_STR2, token[i + 1][0])))
+		return (printf("2\n"), perror_token(token[i + 1], SYN_ERR), true);
+	if (token[i] && token[i][0] == ')' && token[i + 1]
+		&& (token[i + 1][0] == '(' || !ft_strchr(OP_STR, token[i + 1][0])))
 		return (printf("3\n"), perror_token(token[i + 1], SYN_ERR), true);
 	if (token[i] && token[i][0] == '(' && token[i + 1]
 			&& token[i + 1][0] == ')')
