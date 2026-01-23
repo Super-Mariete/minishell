722a2cc44eb6796ccce4fa29dc0527c3eec10c99
rms35 <martinsoeder35@gmail.com>
2026-01-23 22:48:39 +0100

    eliminada la basura

--- test_fds.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in {1..100}; do
-    echo "Test $i"
-    echo "echo test$i | cat | cat | cat | cat | cat" | ./minishell
-done
-echo "Si llega aquí sin errores, no hay leaks de FD"
